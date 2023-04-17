9e44a853763bc072df4adc2f603bbdb06e98e649
Gihun Ham <ham@e-mirim.hs.kr>
2023-04-17 12:18:29 +0900

    여러가지 콘솔 메서드를 사용해서 스네이크 움직임 구현

--- ConsoleApp/Program.cs
+++ ConsoleApp/Program.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Security.Cryptography.X509Certificates;
 using System.Text;
 using System.Threading;
 
@@ -8,15 +9,38 @@ namespace ConsoleApp
 {
     internal class Program
     {
+        private static readonly int SnakeStateTotal = 3;
+        private const int SnakeState1 = 0;
+        private const int SnakeState2 = 1;
+        private const int SnakeState3 = 2;
+        private static readonly int SnakeMoveCount = 50;
+        private static readonly int SnakeRow = 5;
+        private static readonly int SnakeSleepTime = 1000;
+
         static void Main(string[] args)
         {
-            Console.Write("메서드 호출");
-            Thread.Sleep(1000);
-            Console.SetCursorPosition(5, 5);
-            Console.Write("메서드 호출2");
-            Thread.Sleep(2000);
-            Console.SetCursorPosition(0, 0);
-            Console.Write("메서드 호출3");
+            int x = SnakeState1;
+            while (x < SnakeMoveCount)
+            {
+                Console.Clear();
+                Console.SetCursorPosition(x, SnakeRow);
+
+                switch (x % SnakeStateTotal)
+                {
+                    case SnakeState1:
+                        Console.WriteLine("__@");
+                        break;
+                    case SnakeState2:
+                        Console.WriteLine("_^@");
+                        break;
+                    case SnakeState3:
+                        Console.WriteLine("^_@");
+                        break;
+                }
+
+                Thread.Sleep(SnakeSleepTime);
+                x++;
+            }
         }
     }
 }
